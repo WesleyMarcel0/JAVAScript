fe3843ceb6f4af7f96872204d1d503c331a5a775
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2022-11-30 00:33:18 -0300

    ex8_9_10e11

---
+++ Cursojr/aula01/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+
+console.log('a velocidade do seu carro é ' + vel)
+
+if(vel > 60){ //condição simplis pois não tem o não
+    console.log('Você ultrapassou a velocidade permitida')
+}
---
+++ Cursojr/aula01/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'estados unidos'
+
+if( pais != 'Brasil'){
+    console.log(' vc é brasileiro')
+} else {
+    console.log('vc é Estrangeiro')
+}
