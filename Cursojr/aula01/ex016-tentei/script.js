60ba95bb02e357f0c59d1d2266e58fe6c00dc6a2
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-04-17 16:29:46 -0300

    ex16

--- Cursojr/aula01/ex016-tentei/script.js
+++ Cursojr/aula01/ex016-tentei/script.js
@@ -1,15 +1,39 @@
 
 function contar() {
 
-    let inicio = document.getElementById('txti') //a variavel let não vai existir quando terminaar a função
-    let fim = document.getElementeById('txtf') //economiza memoria
-    let passo = document.getElementById('txtp')
+    let inicio = document.querySelector('input#txti') //a variavel let não vai existir quando terminaar a função
+    let fim = document.querySelector('input#txtf') //economiza memoria
+    let passo = document.querySelector('input#txtp')
+    let res = document.querySelector('div#res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         //se o valor de INICIO ou FIM ou PASSO for igual a 0
-        window.alert('[ERRO]FALTÃO DADOS')
+        //window.alert('[ERRO]FALTÃO DADOS')
+        res.innerHTML = 'Impossivel contar.'
     }else {
-        window.alert('tudo certo')
+       res.innerHTML = 'Contando... <br>'
+       let i = Number(inicio.vlue)
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+        if (p = 0){
+            window.alert('passo invalido! Considerando passo 1')
+        }
+
+       if(i > f){
+       for(let c = 1 ; c <= f ; c += p) {
+        //contagem crescente
+        res.innerHTML += (c +'\u{2764}')
+       }
+        
+     } else {
+        for(let c = i; c >= f; c-=p) {
+        //contagem regressiva
+        res.innerHTML += (c + '\u{2764}')
+        }
+
+     }
+
+      res.innerHTML += '\u{1F3C1}'
     }
 }
 
